72aee49de83bbd6d2d4c60981d6610bdf7c1aeaa
brandini <bnguyen3938@sdsu.edu>
2025-01-25 13:59:38 -0800

    user-authentication

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
+import bcrypt from "bcrypt";
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("views"));
 app.use(express.static("public"));
 app.use(express.static("views/partials"));
 app.set("view engine", "ejs");
+
+const users = [];
+
+// Route: Add a new user with hashed password
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = { name: req.body.name, password: hashedPassword };
+    users.push(user);
+    res.status(201).send("User created successfully");
+  } catch {
+    res.status(500).send("Internal server error");
+  }
+});
+
+// Route: Authenticate user login
+app.post('/users/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send('Cannot find user');
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success');
+    } else {
+      res.send('Not Allowed');
+    }
+  } catch {
+    res.status(500).send("Internal server error");
+  }
+});
+
+// Route: Get all users
+app.get('/users', (req, res) => {
+  res.json(users);
+});
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -20,5 +60,5 @@ app.get("/start", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Listening on port 3000");
+  console.log(`Listening on port ${port}`);
 });
