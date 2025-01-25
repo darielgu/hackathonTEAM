29a93eef5a215afed0fcaeb13dbdd0bfae3e68a9
Dariel <darielgutierrez332@gmail.com>
2025-01-25 10:20:19 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+This Is the beginning of my Blog Website. To get it started:
+
+1. Run npm i (In Terminal)
+2. Head to LocalHost 3000
---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("views"));
+app.use(express.static("public"));
+app.use(express.static("views/partials"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.listen(port, () => {
+  console.log("Listening on port 3000");
+});
