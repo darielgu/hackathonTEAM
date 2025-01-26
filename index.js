8ccf08654883299cfa83280e6e23aba7e2e9b3ab
Dariel <darielgutierrez332@gmail.com>
2025-01-25 16:39:52 -0800

    did a bunch of stuff, graphs now work

--- index.js
+++ index.js
@@ -15,20 +15,57 @@ app.get("/", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
-app.get("/start", (req, res) => {
-  res.render("auth.ejs");
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
+//When the user clicks on the register button, this route will be called and send them to the Log in page
 app.post("/register", (req, res) => {
   const { email, password } = req.body;
   console.log(`Username: ${email}, Password: ${password}`);
-  res.redirect("/start");
+  res.redirect("/login");
 });
+//Whenever the user clicks on the login button, this route will be called and send them to the start dashboard page
 app.post("/auth", (req, res) => {
   {
-    res.render("start.ejs");
+    res.render("dashboard.ejs");
+  }
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard", { user: null, error: null });
+});
+
+app.post("/dashboard", (req, res) => {
+  const userId = req.body.userId;
+  const user = users[userId];
+
+  if (user) {
+    res.render("dashboard", { user, error: null });
+  } else {
+    res.render("dashboard", {
+      user: null,
+      error: "User not found. Please try again.",
+    });
   }
 });
 
 app.listen(port, () => {
   console.log("Listening on port 3000");
 });
+
+//dummy data
+const users = {
+  12345: {
+    name: "John Doe",
+    email: "john.doe@example.com",
+    accountBalance: 150000,
+    portfolio: "Stocks, Bonds, ETFs",
+    lastTransaction: "2025-01-15 | Stock Purchase | $5,000",
+    transactions: [
+      { date: "2025-01-10", description: "Groceries", amount: 150 },
+      { date: "2025-01-12", description: "Rent", amount: 2000 },
+      { date: "2025-01-15", description: "Investment", amount: 5000 },
+      { date: "2025-01-20", description: "Utilities", amount: 100 },
+    ],
+  },
+};
